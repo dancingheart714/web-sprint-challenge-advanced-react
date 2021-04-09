a8460112ce2c8c273ba492fa77323a7264998b71
Leah King <dancingheart714@yahoo.com>
2021-04-09 11:21:31 -0400

    plants showing, cart working

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,1 +1,17 @@
 // write your custom hook here to control your checkout form
+import { useState } from 'react';
+
+const useForm = (initialValues) => {
+  const [values, setValue] = useState('form', initialValues);
+
+  const handleChanges = (e) => {
+    setValue({
+      ...values,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  return [values, handleChanges];
+};
+
+export default useForm;
